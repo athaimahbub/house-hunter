4aadd88ff5f3d47c308a6375418e63271790d12c
athaimahbub <athaimahbub2016@gmail.com>
2023-07-17 22:32:56 +0600

    Navbar and Footer added

---
+++ src/Pages/Footer.jsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer footer-center p-4 bg-blue-700 text-white font-semibold">
+                <div>
+                    <p>Copyright © 2023 - All right reserved by House Hunter</p>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
